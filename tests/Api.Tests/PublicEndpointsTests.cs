56f491d1cfdbeb05aa219534adacec20d30e896a
Lee Moody <lee.moody@esynergy.co.uk>
2026-07-22 16:04:56 +0100

    Remediate public endpoint unit test failures

--- tests/Api.Tests/PublicEndpointsTests.cs
+++ tests/Api.Tests/PublicEndpointsTests.cs
@@ -8,7 +8,6 @@ using System.Reflection;
 using Lis.Infra.FeatureFlag.Api.Endpoints.Public;
 using Lis.Infra.FeatureFlag.Models.Requests;
 using Lis.Infra.FeatureFlag.Models.Responses;
-using Lis.Infra.FeatureFlag.Models.Responses.Common;
 using Lis.Infra.FeatureFlag.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
@@ -31,29 +30,35 @@ public class PublicEndpointsTests
             .SelectMany(dataSource => dataSource.Endpoints)
             .OfType<RouteEndpoint>()
             .ToList();
+
         var routePatterns = endpoints
             .Select(endpoint => endpoint.RoutePattern.RawText)
             .OrderBy(pattern => pattern)
             .ToList();
 
         endpoints.Count.ShouldBe(2);
+
         routePatterns.ShouldBe(
         [
-            "evaluate/{environment:regex(^(?i)(DEV|TEST|EXT-TEST|PROD)$)}/{group}",
-            "evaluate/{environment:regex(^(?i)(DEV|TEST|EXT-TEST|PROD)$)}/{group}/{flag}",
+            "evaluate/{groupName}",
+            "evaluate/{groupName}/{flagName}",
         ]);
-        endpoints.All(endpoint => endpoint.Metadata.GetMetadata<IEndpointNameMetadata>()?.EndpointName ==
-                                  OpenApiMetadata.GetFeatureFlagGroupStatusRoute.Name)
-            .ShouldBeTrue();
-        endpoints.All(endpoint => endpoint.Metadata.GetMetadata<IEndpointSummaryMetadata>()?.Summary ==
-                                  OpenApiMetadata.GetFeatureFlagGroupStatusRoute.Summary)
-            .ShouldBeTrue();
-        endpoints.All(endpoint => endpoint.Metadata.GetMetadata<IEndpointDescriptionMetadata>()?.Description ==
-                                  OpenApiMetadata.GetFeatureFlagGroupStatusRoute.Description)
-            .ShouldBeTrue();
-        endpoints.All(endpoint =>
-                endpoint.Metadata.GetMetadata<HttpMethodMetadata>()!.HttpMethods.Single() == HttpMethods.Get)
-            .ShouldBeTrue();
+
+        endpoints[0].Metadata.GetMetadata<IEndpointNameMetadata>()?.EndpointName
+            .ShouldBe(OpenApiMetadata.GetFeatureFlagGroupStatusRoute.Name);
+        endpoints[0].Metadata.GetMetadata<IEndpointSummaryMetadata>()?.Summary
+            .ShouldBe(OpenApiMetadata.GetFeatureFlagGroupStatusRoute.Summary);
+        endpoints[0].Metadata.GetMetadata<IEndpointDescriptionMetadata>()?.Description
+            .ShouldBe(OpenApiMetadata.GetFeatureFlagGroupStatusRoute.Description);
+        endpoints[0].Metadata.GetMetadata<HttpMethodMetadata>()!.HttpMethods.Single().ShouldBe(HttpMethods.Get);
+
+        endpoints[1].Metadata.GetMetadata<IEndpointNameMetadata>()?.EndpointName
+            .ShouldBe(OpenApiMetadata.GetFeatureFlagStatusRoute.Name);
+        endpoints[1].Metadata.GetMetadata<IEndpointSummaryMetadata>()?.Summary
+            .ShouldBe(OpenApiMetadata.GetFeatureFlagStatusRoute.Summary);
+        endpoints[1].Metadata.GetMetadata<IEndpointDescriptionMetadata>()?.Description
+            .ShouldBe(OpenApiMetadata.GetFeatureFlagStatusRoute.Description);
+        endpoints[1].Metadata.GetMetadata<HttpMethodMetadata>()!.HttpMethods.Single().ShouldBe(HttpMethods.Get);
     }
 
     [Fact]
@@ -61,18 +66,24 @@ public class PublicEndpointsTests
     {
         var service = Substitute.For<IFeatureFlagService>();
         service.GetFeatureFlagStatus(Arg.Any<GetFeatureFlagStatus>(), Arg.Any<CancellationToken>())
-            .Returns(new FeatureFlagStatusResult() { FlagName = "Test", FlagEnabled = true, Success = true });
-        var method = typeof(PublicEndpoints).GetMethod("EvaluateFeatureFlagStatusByGroupAndFlag",
+            .Returns(new FeatureFlagStatusResult() { FlagName = "NewUi", FlagEnabled = true, Success = true });
+
+        var method = typeof(PublicEndpoints).GetMethod(
+            "GetFeatureFlagStatusRoute",
             BindingFlags.NonPublic | BindingFlags.Static);
+
         var request =
             new GetFeatureFlagStatus { GroupName = "Payments", FlagName = "NewUi", EnvironmentName = "Prod", };
 
         method.ShouldNotBeNull();
 
-        var task = (Task<IResult>)method!.Invoke(null, [request, service, TestContext.Current.CancellationToken])!;
+        var task = (Task<IResult>)method.Invoke(null, [request, service, TestContext.Current.CancellationToken])!;
         var result = await task;
-        var okResult = result.ShouldBeOfType<Ok<FeatureFlagStatus>>();
+        var okResult = result.ShouldBeOfType<Ok<FeatureFlagStatusResult>>();
 
-        okResult.Value.ShouldBe(new FeatureFlagStatusResult { FlagName = "Test", FlagEnabled = true, Success = true });
+        okResult.Value.ShouldNotBeNull();
+        okResult.Value.FlagName.ShouldBe("NewUi");
+        okResult.Value.FlagEnabled.ShouldBeTrue();
+        okResult.Value.Success.ShouldBeTrue();
     }
 }
